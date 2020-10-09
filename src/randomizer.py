1802a1959f07a3fe6671e8e0b9a7b95a82b848ce
JudeLakkis <JudeLakkis123@gmail.com>
2020-10-09 22:04:29 +0200

    Order issue

--- src/randomizer.py
+++ src/randomizer.py
@@ -11,25 +11,25 @@ class KartStructure():
             data = json.load(f)
         self.character = [i['name'] for i in data['part_dict']['char']]
         self.body = [i['name'] for i in data['part_dict']['body']]
-        self.glider = [i['name'] for i in data['part_dict']['glider']]
         self.tire = [i['name'] for i in data['part_dict']['tire']]
+        self.glider = [i['name'] for i in data['part_dict']['glider']]
         self.setup = []
         self.kartcode = None
 
     def generate_setup(self):
         char = self.character[randint(0, len(self.character) - 1)]
         body = self.body[randint(0, len(self.body) - 1)]
-        glider = self.glider[randint(0, len(self.glider) - 1)]
         tire = self.tire[randint(0, len(self.tire) - 1)]
-        self.setup = [char, body, glider, tire]
+        glider = self.glider[randint(0, len(self.glider) - 1)]
+        self.setup = [char, body, tire, glider]
         return self.setup
 
     def generate_code(self):
         character = str(self.character.index(self.setup[0]))
         body = str(self.body.index(self.setup[1]))
-        glider = str(self.glider.index(self.setup[2]))
         tire = str(self.tire.index(self.setup[3]))
-        self.kartcode = '.'.join([character, body, glider, tire])
+        glider = str(self.glider.index(self.setup[2]))
+        self.kartcode = '.'.join([character, body, tire, glider])
         return self.kartcode
 
 # example = KartStructure()
